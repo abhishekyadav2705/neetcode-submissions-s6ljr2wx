8ec65ce311d5295a11c20ea2917abdac795f6b41
abhishekyadav2705 <67361957+abhishekyadav2705@users.noreply.github.com>
2026-06-09 17:12:41 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int answer=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==answer){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
